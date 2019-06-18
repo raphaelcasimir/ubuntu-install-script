6ea432417d491e9dccc3724b48f15e52ccd01ff3
sagan <mail@raph.io>
2019-06-19 01:48:44 +0200

    Updated apty

--- ubuntu_install_script.sh
+++ ubuntu_install_script.sh
@@ -83,7 +83,7 @@ sudo apt install obs-studio
 
 # Spotify
 ## 1. Add the Spotify repository signing keys to be able to verify downloaded packages
-sudo apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv-keys 931FF8E79F0876134EDDBDCCA87FF9DF48BF1C90
+sudo apt-key adv -y --keyserver hkp://keyserver.ubuntu.com:80 --recv-keys 931FF8E79F0876134EDDBDCCA87FF9DF48BF1C90
 
 ## 2. Add the Spotify repository
 echo deb http://repository.spotify.com stable non-free | sudo tee /etc/apt/sources.list.d/spotify.list
@@ -92,7 +92,7 @@ echo deb http://repository.spotify.com stable non-free | sudo tee /etc/apt/sourc
 sudo apt-get update
 
 ## 4. Install Spotify
-sudo apt-get install spotify-client
+sudo apt-get install -y spotify-client
 
 # Task manager that allows you to easily kill Apps by selecting them on the screen
 apty xfce4-taskmanager
